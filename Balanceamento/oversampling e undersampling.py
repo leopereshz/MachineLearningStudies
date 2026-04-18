ef6643f8ccfa387239afd513db6021a0dadeefea
Leonardo Peres <38218066+leopereshz@users.noreply.github.com>
2026-04-17 23:11:00 -0300

    balancing

---
+++ Balanceamento/oversampling e undersampling.py
@@ -0,0 +1,40 @@
+#EXEMPLO GERAL DE DESBALANCEADOS
+from sklearn.datasets import make_classification
+X, y = make_classification(
+    n_classes=2, class_sep=1.5, weights=[0.99, 0.01],
+    n_informative=3, n_redundant=1, flip_y=0,
+    n_features=20, n_clusters_per_class=1,
+    n_samples=10000, random_state=10
+)
+df = pd.DataFrame(X)
+df['target'] = y
+    
+plot_2d_space(X, y, 'Exemplo de desbalanceamento')
+
+#Esse tipo de situação é muito comum devido ao fato da classificação minoritária,
+# representar normalmente cerca de 2–5% do todo.
+
+
+target_count = df_train.target.value_counts()
+print('Class 0:', target_count[0])
+print('Class 1:', target_count[1])
+print('Proportion:', round(target_count[0] / target_count[1], 2), ': 1')
+target_count.plot(kind='bar', title='Count (target)',color = ['#1F77B4', '#FF7F0E']);
+
+
+#UNDERSAMPLING = Diminui os registros da classe maior para ficarem na mesma quantidade da menor.
+
+# CONTAR CLASSES
+count_class_0, count_class_1 = df_train.target.value_counts()
+# Divide by class
+df_class_0 = df_train[df_train['target'] == 0]
+df_class_1 = df_train[df_train['target'] == 1]
+df_class_0_under = df_class_0.sample(count_class_1)
+df_test_under = pd.concat([df_class_0_under, df_class_1], axis=0)
+print('Random under-sampling:')
+print(df_test_under.target.value_counts())
+df_test_under.target.value_counts().plot(kind='bar', title='Count (target)',color = ['#1F77B4', '#FF7F0E']);
+
+
+#OVERSAMPLING = Aumenta os registros da classe menor para ficarem na mesma quantidade da maior.
+
